f2f8d775168f573332886fb775358d9fd9e65787
Alireza <alireza.bayrami@gmail.com>
2024-07-20 13:11:20 +0330

    1.php file in if and else exercise updated

--- week_1/if and loops/1.php
+++ week_1/if and loops/1.php
@@ -1,10 +1,13 @@
 <?php
 $correct_degrees = true;
-$all_degrees = [70, 80, 30];
+$all_degrees = [250, 10, 300];
 $sum_of_all_degrees = 0;
 foreach ($all_degrees as $value) {
     if ($value != 0) {
-        $sum_of_all_degrees += $value;
+        if ($value > 180) {
+            $value -= 360;
+        }
+        $sum_of_all_degrees += abs($value);
     }
     else {
         $correct_degrees = false;
